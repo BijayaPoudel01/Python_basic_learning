5a33aeb5345cfa169cf7938bfbbfaaa39eb4fb7a
BijayaPoudel01 <bijayapoudel32@gmail.com>
2025-01-09 07:53:46 +0545

    Learned about If statement.

---
+++ python_basic03.py
@@ -0,0 +1,10 @@
+       #If Statement
+temperature = 14
+if temperature > 30:
+           print("It's a hot day")
+elif temperature > 20: #(more than 20 less than or equal to 30)
+    print("It's a nice day")
+elif temperature > 15:
+    print("It's a bit cold day")
+else:
+    print("It's a cold day")
