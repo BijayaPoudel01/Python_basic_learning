671000c37373ab9ed0a9f98d26b15a067b6bd011
BijayaPoudel01 <bijayapoudel32@gmail.com>
2025-01-10 08:04:22 +0545

    Learned about range function, tuples and set.

---
+++ python_basics06.py
@@ -0,0 +1,13 @@
+#The range Function
+numbers = range(6,13,2) # 2 is used as jumping 2 step like 6,8,10 etc.
+for number in numbers:
+    print(number)
+
+    #tuples. note: we use small bracket () in tuples, whereas we use big bracket[] in lists and if we use curly bracket it's set.
+    numbers = (1,2,3,4,5)
+    #numbers[0] = 9
+    #print(numbers[0]) if we try this its shows error i.e, tuples is not changeable like lists.
+
+#set
+s={22,11,10,30}
+print(s)
