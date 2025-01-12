156c7b6b6a45ee8eaf73cee61443992d850e9195
BijayaPoudel01 <bijayapoudel32@gmail.com>
2025-01-12 20:43:38 +0545

    Build basic calculator.

---
+++ python_basic10.py
@@ -0,0 +1,15 @@
+# Basic calculator with what i know.
+num1 = float(input("enter first number"))
+op = input("enter operator")
+num2 = float(input("enter second number"))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == "/":
+    print(num1 / num2)
+else:
+    print("Invalid Operator")
